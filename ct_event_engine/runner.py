dfe9e42d889ffdae997a7ce738ecf642ca58078d
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-02-14 16:15:01 -0500

    Update Docker Build Process

--- ct_event_engine/runner.py
+++ ct_event_engine/runner.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from calendar_event_engine.Runner import start_event_engine
@@ -8,16 +9,21 @@ from slack_sdk.webhook import WebhookClient
 
 from ct_event_engine.Websites.cafe9 import Cafe9Scraper
 
+logger = logging.Logger(__name__)
+
 if __name__ == "__main__":
     env_webhook = None if os.environ.get("SLACK_WEBHOOK") is None else WebhookClient(os.environ.get("SLACK_WEBHOOK"))
     env_test_mode = False if "TEST_MODE" not in os.environ else True
     submission_json_path = os.getenv("RUNNER_SUBMISSION_JSON_PATH")
+    logger.info("Got Env Variables")
     cache_db = SQLiteDB(env_test_mode)
+    logger.info("Connect to DB")
     custom_scrapers = {
         MobilizonUploader(env_test_mode, cache_db) : [
             CustomScraperJob("Cafe 9", "Music venue", Cafe9Scraper())
         ]
     }
+    logger.info("Engine starting.")
     start_event_engine(submission_json_path, cache_db, env_webhook, custom_scrapers, env_test_mode)
 
 
